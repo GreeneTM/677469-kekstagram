30272e6193c94585313a6acb041b0855da2a026d
GreeneTM <d.greenetm@gmail.com>
2018-12-15 14:45:49 +0300

    get data from the server as an array with обесц / получаем данные с сервера в виде массива с объектами

--- js/backend.js
+++ js/backend.js
@@ -1,4 +1,15 @@
 'use strict';
 (function () {
+  var xhr = new XMLHttpRequest();
+  console.log('xhr.readyState', xhr.readyState);
+  xhr.responseType = 'json';
 
+  xhr.addEventListener('load', function (evt) {
+    console.log(evt.target === xhr);
+    console.log(xhr.response);
+  });
+  xhr.open('GET', ' https://js.dump.academy/kekstagram/data');
+  console.log('xhr.readyState', xhr.readyState);
+
+  xhr.send();
 })();
