f1c4780b340c3967821b327d1822107b22c0733e
GreeneTM <d.greenetm@gmail.com>
2018-12-15 17:23:01 +0300

    Функция получения данных

--- js/backend.js
+++ js/backend.js
@@ -1,8 +1,8 @@
 'use strict';
 (function () {
-  var xhr = new XMLHttpRequest();
-  var URL = ' https://js.dump.academy/kekstagram/data';
-
+  var URL = {
+    DOWLOAD: 'https://js.dump.academy/kekstagram/data'
+  };
 
   var setup = function (xhr, onLoad, onError) {
     xhr.responseType = 'json';
@@ -13,7 +13,31 @@
         onError('ошибка загрузки');
       }
     });
-    xhr.open('GET', URL);
+  };
+
+  var download = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    setup(xhr, onLoad, onError);
+    xhr.open('GET', URL.DOWLOAD);
     xhr.send();
   };
+
+  window.backend = {
+    download: download
+  };
 })();
+// var xhr = new XMLHttpRequest();
+//
+// var URL = {
+//   DOWLOAD: 'https://js.dump.academy/kekstagram/data',
+//   UPLOAD: 'https://js.dump.academy/kekstagram'
+// };
+// xhr.addEventListener('load', function (evt) {
+//   console.log(evt.target === xhr);
+//   console.log(xhr);
+//   console.log(xhr.response[0].likes);
+// });
+// xhr.responseType = 'json';
+// xhr.open ('GET', URL.DOWLOAD);
+//
+// xhr.send();
