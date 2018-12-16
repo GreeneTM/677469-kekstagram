eaf6235811abdeabcf4e10f25882bc47c57d5af0
GreeneTM <d.greenetm@gmail.com>
2018-12-16 23:43:28 +0300

    corrections after working with a mentor / исправления после работы с наставником

--- js/backend.js
+++ js/backend.js
@@ -1,17 +1,25 @@
 'use strict';
 (function () {
   var URL = {
-    DOWLOAD: 'https://js.dump.academy/kekstagram/data'
+    DOWLOAD: 'https://js.dump.academy/kekstagram/data',
+    UPLOAD: 'https://js.dump.academy/kekstagram'
   };
 
   var setup = function (xhr, onLoad, onError) {
     xhr.responseType = 'json';
+    xhr.timeout = 10000;
     xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
         onLoad(xhr.response);
       } else {
         onError('ошибка загрузки');
       }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Ошибка ответа: ' + xhr.status + ' ' + xhr.statusText);
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
   };
 
@@ -22,22 +30,15 @@
     xhr.send();
   };
 
+  var upload = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    setup(xhr, onLoad, onError);
+    xhr.open('POST', URL.UPLOAD);
+    xhr.send(data);
+  };
   window.backend = {
-    download: download
+    download: download,
+    upload: upload
   };
 })();
-// var xhr = new XMLHttpRequest();
-//
-// var URL = {
-//   DOWLOAD: 'https://js.dump.academy/kekstagram/data',
-//   UPLOAD: 'https://js.dump.academy/kekstagram'
-// };
-// xhr.addEventListener('load', function (evt) {
-//   console.log(evt.target === xhr);
-//   console.log(xhr);
-//   console.log(xhr.response[0].likes);
-// });
-// xhr.responseType = 'json';
-// xhr.open ('GET', URL.DOWLOAD);
-//
-// xhr.send();
+
--- js/pictures.js
+++ js/pictures.js
@@ -1,95 +1,48 @@
 'use strict';
 (function () {
-  var NUMBER_OF_PHOTOS = 25;
-
-  var commentsList = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  ];
-
-  var descriptionList = [
-    'Тестим новую камеру!',
-    'Затусили с друзьями на море',
-    'Как же круто тут кормят',
-    'Отдыхаем...',
-    'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
-    'Вот это тачка!'
-  ];
-
-  var randomElementFromArray = function (items) {
-    return items[Math.floor(Math.random() * items.length)];
-  };
-
-  var randomInteger = function (min, max) {
-    var rand = Math.floor(min + Math.random() * (max + 1 - min));
-    return rand;
-  };
-
-  var makesComment = function () {
-    var minCommentValue = 1;
-    var maxCommentValue = 2;
-    var lineComments;
-    if (randomInteger(minCommentValue, maxCommentValue) > minCommentValue) {
-      lineComments = randomElementFromArray(commentsList) + ' ' + randomElementFromArray(commentsList);
-      return lineComments;
-    } lineComments = randomElementFromArray(commentsList);
-    return lineComments;
-  };
-
-  var creatingComments = function () {
-    var comments = [];
-    for (var i = 0; i < randomInteger(1, 6); i++) {
-      comments[i] = makesComment();
-    }
-    return comments;
-  };
-
-  var generatesAnArray = function () {
-    var photos = [];
-    for (var i = 0; i < NUMBER_OF_PHOTOS; i++) {
-      photos[i] = {
-        url: 'photos/' + (i + 1) + '.jpg',
-        likes: randomInteger(15, 200),
-        comments: creatingComments(),
-        description: randomElementFromArray(descriptionList)
-      };
-    }
-    return photos;
-  };
-
-  var photos = generatesAnArray();
 
   var pictures = document.querySelector('.pictures');
   var templatePictures = document.querySelector('#picture').content.querySelector('a');
   var fragment = document.createDocumentFragment();
-  window.bigPicture = document.querySelector('.big-picture');
-  var bigPictureImg = window.bigPicture.querySelector('img');
-  var likesCount = window.bigPicture.querySelector('.likes-count');
-  var socialCaption = window.bigPicture.querySelector('.social__caption');
-  var commentsCount = window.bigPicture.querySelector('.comments-count');
-  var socialComments = window.bigPicture.querySelector('.social__comments');
+  var bigPicture = document.querySelector('.big-picture');
+  var bigPictureImg = bigPicture.querySelector('img');
+  var likesCount = bigPicture.querySelector('.likes-count');
+  var socialCaption = bigPicture.querySelector('.social__caption');
+  var commentsCount = bigPicture.querySelector('.comments-count');
+  var socialComments = bigPicture.querySelector('.social__comments');
 
-  for (var i = 0; i < photos.length; i++) {
-    var indexPhotos = photos[i];
-    var element = templatePictures.cloneNode(true);
-    element.querySelector('img').src = indexPhotos.url;
-    element.querySelector('.picture__likes').textContent = indexPhotos.likes;
-    element.querySelector('.picture__comments').textContent = indexPhotos.comments.length;
+  var photoClickHandle = function (photos, evt) {
+    var index = evt.currentTarget.dataset.index;
+    var currentPhoto = photos[index];
+    bigPictureImg.src = currentPhoto.url;
+    socialCaption.textContent = currentPhoto.descriptionList;
+    commentsCount.textContent = currentPhoto.comments.length;
+    likesCount.textContent = currentPhoto.likes;
+    socialComments.innerHTML = '';
+    for (var j = 0; j < currentPhoto.comments.length; j++) {
+      var currentComment = currentPhoto.comments[j];
+      socialComments.insertAdjacentHTML('beforeend', '<li class="social__comment">\n <img class="social__picture" src="' + currentComment.avatar + '"\n alt="Аватар комментатора фотографии"\n width="35" height="35">\n <p class="social__text">' + currentComment.message + '</p>\n </li>');
 
-    element.addEventListener('click', function (evt) {
-      bigPictureImg.src = evt.target.src;
-      socialCaption.textContent = randomElementFromArray(descriptionList);
-      commentsCount.textContent = evt.srcElement.nextElementSibling.children[0].textContent;
-      likesCount.textContent = evt.srcElement.nextElementSibling.children[1].textContent;
-      socialComments.insertAdjacentHTML('beforeend', '<li class="social__comment">\n <img class="social__picture" src="img/avatar-' + randomInteger(1, 6) + '.svg"\n alt="Аватар комментатора фотографии"\n width="35" height="35">\n <p class="social__text">' + randomElementFromArray(commentsList) + '</p>\n </li>');
-      window.bigPicture.classList.remove('hidden');
-    });
-    fragment.appendChild(element);
-  }
-  pictures.appendChild(fragment);
+    }
+    window.bigPicture.classList.remove('hidden');
+  };
+  var renderPhotos = (function (photos) {
+    for (var i = 0; i < photos.length; i++) {
+      var currentPhoto = photos[i];
+      var element = templatePictures.cloneNode(true);
+      element.dataset.index = i;
+      element.querySelector('img').src = currentPhoto.url;
+      element.querySelector('.picture__likes').textContent = currentPhoto.likes;
+      element.querySelector('.picture__comments').textContent = currentPhoto.comments.length;
+
+      element.addEventListener('click', function (evt) {
+        photoClickHandle(photos, evt);
+      });
+      fragment.appendChild(element);
+    }
+    pictures.appendChild(fragment);
+  });
+  window.backend.download(renderPhotos);
+  window.bigPicture = bigPicture;
 })();
 
